0ac0aa378c543012a677407db978c032cea6bea8
farzad <farzaddarvish@yahoo.com>
2024-01-06 00:38:52 +0330

    Make some new endpoints

--- ticket_volley/views.py
+++ ticket_volley/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from django.contrib.auth import get_user_model
+from .models import Stadium, Match, Seat
+from .serializers import UserSerializer, StadiumSerializer, MatchSerializer, SeatSerializer
 
-# Create your views here.
+
+User = get_user_model()
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+class StadiumViewSet(viewsets.ModelViewSet):
+    queryset = Stadium.objects.all()
+    serializer_class = StadiumSerializer
+
+class MatchViewSet(viewsets.ModelViewSet):
+    queryset = Match.objects.all()
+    serializer_class = MatchSerializer
+
+class SeatViewSet(viewsets.ModelViewSet):
+    queryset = Seat.objects.all()
+    serializer_class = SeatSerializer
