59a52d84814b451c5b73d50d3047d0e57f77f0f7
farzad <farzaddarvish@yahoo.com>
2024-01-06 00:39:43 +0330

    Chage the name

--- ticket_volley/apps.py
+++ ticket_volley/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class TicketvolleyConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "TicketVolley"
+    name = "ticket_volley"
